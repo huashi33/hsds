7f9e4b0369fc089c23f997bd823107d8608d4a32
huashi33 <huashi33@foxmail.com>
2025-09-06 11:01:00 +0800

    change gtest-lib path

---
+++ arm32.cmake
@@ -0,0 +1,5 @@
+set(ARM32_COMPILER_DIR "/home/huashi/mysoft/a40i-hmi/ext-toolchain-it7070ei/bin")
+set(CMAKE_C_COMPILER ${ARM32_COMPILER_DIR}/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${ARM32_COMPILER_DIR}/arm-linux-gnueabihf-g++)
+set(CMAKE_C_COMPILER_TARGET "arm-linux-gnueabihf")
+
